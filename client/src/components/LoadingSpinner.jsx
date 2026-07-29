6c02d551c8f975be1a6e89517766c33ff416957a
MP <cdeveloper2016@gmail.com>
2026-07-29 18:36:25 +0530

    Remove client folder

--- client/src/components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex items-center justify-center py-12">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary-600"></div>
-    </div>
-  )
-}
-
-export default LoadingSpinner
